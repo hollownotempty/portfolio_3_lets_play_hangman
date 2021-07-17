08a5b01ca59a5b3264d1369203f6058bcc3b6adf
Tom Holohan <thomasholohan8@gmail.com>
2021-07-17 12:52:22 +0000

    Add main() function to begin game cycle

--- run.py
+++ run.py
@@ -1,11 +1,22 @@
-# python code goes here
+# import random word package
 from random_word import RandomWords
 r = RandomWords()
 
 
+# grab random word to begin the game with
 def get_word():
     word = r.get_random_word()
     return word.upper()
+
+
+# sets state of play and displays the blank spaces for the player to guess
+def play(word):
+    word_completed = "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
+    print("Let's Play Hangman")
 
 
 def display_hangman(tries):
@@ -81,3 +92,11 @@ def display_hangman(tries):
                 """
     ]
     return stages[tries]
+
+
+def main():
+    word = get_word()
+    play(word)
+    while input("Play Again? (Y/N) ").upper() == "Y":
+        word = get_word()
+        play(word)
